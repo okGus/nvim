ce7ac1c11117d7a68d91fb9abbc73fb2f4017723
okGus <rosalesagustavo@gmail.com>
2024-12-16 09:39:24 -0500

    Add files via upload

--- lua/supersmol/init.lua
+++ lua/supersmol/init.lua
@@ -1,2 +1,3 @@
+--vim.lsp.set_log_level("debug")
 require("supersmol.remap")
 require("supersmol.set")
--- lua/supersmol/remap.lua
+++ lua/supersmol/remap.lua
@@ -1,10 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- select a section of text and move the text
+-- keeping the text context pretty cool
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -12,8 +14,18 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
 -- to comment or uncomment, Ctrl-c to Esc instead of using Esc
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 --Nvimtree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
+
+-- Indent with Tab and un-indent with Shift + Tab in visual mode
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+
